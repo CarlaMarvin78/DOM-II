771a1a23eafae7cbae8d9b304564d25e4cef25d3
Carla Marvin <carlamarvin78@gmail.com>
2019-06-18 18:56:34 -0400

    Fourth Commit

--- js/index.js
+++ js/index.js
@@ -4,13 +4,14 @@ let logo_heading = document.querySelector(".logo-heading");
 logo_heading.addEventListener("mouseover", function(event) {
     logo_heading.setAttribute("style","font-size: 7rem; color: hotpink");
 });
+
 logo_heading.addEventListener("mouseout", function(event) {
     logo_heading.removeAttribute("style");
 });
 
 let btn = document.querySelectorAll(".btn");
 btn[0].addEventListener("click", function (event){
-    btn[0].setAttribute("style", "background-color:orange");
+    btn[0].setAttribute("style", "background-color: ");
 });
 
 btn[1].addEventListener("mouseover", function (event){
@@ -25,6 +26,13 @@ btn[2].addEventListener("dblclick", function (event) {
   nav.addEventListener("mouseover", function (event){
       nav.setAttribute("style", "border: 2px dotted blue");
   });
+  let navchildren = nav.children;
+  for(let i=0; i<navchildren.length;++i) {
+    navchildren[i].addEventListener("click", function (event) {
+    return event.preventDefault ();
+});
+};
+
 
 let audio = document.createElement("audio");
 audio.setAttribute("src", "Bus_horn.mp3");
@@ -42,3 +50,5 @@ img_content.addEventListener("mouseover", function (event){
 img_content.addEventListener("mouseout", function (event){
     img_content.setAttribute("src", "img/adventure.jpg");
 });
+
+
