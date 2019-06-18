4c73e6353488bfdd1aad3df199fc8698cb07a0b5
Carla Marvin <carlamarvin78@gmail.com>
2019-06-18 17:16:49 -0400

    Second Commit

--- js/index.js
+++ js/index.js
@@ -1,1 +1,27 @@
 // Your code goes here
+
+let btn = document.querySelectorAll(".btn");
+btn[0].addEventListener("click", function (event){
+    btn[0].setAttribute("style", "background-color:orange");
+});
+
+btn[1].addEventListener("mouseover", function (event){
+    btn[1].setAttribute("style", "border-radius:80px; width:160px; background-color:pink");
+});
+
+btn[2].addEventListener("dblclick", function (event) {
+    btn[2].setAttribute("style", "border: 2px solid red")
+  });
+
+  let nav = document.querySelector(".nav");
+  nav.addEventListener("mouseover", function (event){
+      nav.setAttribute("style", "border: 2px dotted blue");
+  });
+
+let audio = document.createElement("audio");
+audio.setAttribute("src", "Bus_horn.mp3");
+
+let img = document.querySelector(".intro img");
+img.addEventListener("click", function (event){
+    audio.play();
+});
